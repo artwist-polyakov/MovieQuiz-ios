0284ea6132d6ce604c6078d6c20f971a50bc06fe
Aleksandr Poliakov <master@artwist.ru>
2023-04-07 22:11:19 +0300

    Тест на неуспешную работу сетевого клиента сделан

--- MovieQuizTests/MoviesLoaderTests.swift
+++ MovieQuizTests/MoviesLoaderTests.swift
@@ -37,11 +37,24 @@ class MoviesLoaderTests: XCTestCase {
     
     func testFailureLoading() throws {
         // Given
+        let stubNetworkClient = StubNetworkClient(emulateError: true)
+        let loader = MoviesLoader(networkClient: stubNetworkClient)
         
         // When
+        // функция загрузки фильмов асинхронная, нужно ожидание
+        let expectation = expectation(description: "Loading expectation")
         
-        // Then
-        
+        loader.loadMovies { result in
+            //Then
+            switch result {
+            case .success(_):
+                XCTFail("Unexpected failure")
+            case .failure(let error):
+                XCTAssertNotNil(error)
+                expectation.fulfill()
+            }
+        }
+        waitForExpectations(timeout: 1)
         
     }
 }
