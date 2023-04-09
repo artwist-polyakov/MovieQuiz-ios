c98a15d3998ffe5d544da0106a592414f5573fba
Aleksandr Poliakov <master@artwist.ru>
2023-04-09 21:34:05 +0300

    Добавили UI-тест

---
+++ MovieQuizUITests/MovieQuizUITests.swift
@@ -0,0 +1,52 @@
+//
+//  MovieQuizUITests.swift
+//  MovieQuizUITests
+//
+//  Created by Александр Поляков on 09.04.2023.
+//
+
+import XCTest
+
+final class MovieQuizUITests: XCTestCase {
+    var app: XCUIApplication!
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        app = XCUIApplication()
+        app.launch()
+        // это специальная настройка для тестов: если один тест не прошёл,
+        // то следующие тесты запускаться не будут; и правда, зачем ждать?
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        try super.tearDownWithError()
+        app.terminate()
+        app = nil
+    }
+
+    func testExample() throws {
+        
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    
+
+    func testScreenCast() throws {
+        
+        let button = app.buttons["Нет"].tap()
+        
+        
+    }
+
+
+}
